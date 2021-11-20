d51ce106a0052bf91ae7b23bf32e016bc66b63ac
MemeLady <alex.kolpakova2001@gmail.com>
2021-11-20 20:45:44 +0700

    lab 7

---
+++ lab7.js
@@ -0,0 +1,52 @@
+let input = document.getElementById('count');
+
+Figures = classId =>{
+    var val = input.value;
+    for (var i = 0; i < val; i++) 
+    {  
+        var div = document.createElement('div');
+        div.setAttribute('class',classId);
+        div.style.top = String(Math.random()*700+1) + 'px';
+        div.style.left = String(Math.random()*1200+1) + 'px';
+        div.addEventListener('click',changeColorForTriangle);
+        div.addEventListener('dblclick',dissapearFigure);
+    if ( classId == 'square' )
+        {
+            var size = String(Math.random()*125+25) + 'px';
+            div.style.width = div.style.height = size;
+            div.addEventListener('click',changeColor);
+            div.addEventListener('dblclick',dissapearFigure);
+        }
+    if ( classId == 'circle' )
+        {
+            var size = String(Math.random()*125+25) + 'px';
+            div.style.width = div.style.height = size;
+            div.addEventListener('click',changeColor);
+            div.addEventListener('dblclick',dissapearFigure);
+        }
+        document.querySelector("main").appendChild(div);
+    }
+}
+
+changeColorForTriangle = (event) =>
+{
+    r = Math.floor(Math.random() * (256)),
+    g = Math.floor(Math.random() * (256)),
+    b = Math.floor(Math.random() * (256)),
+    color = '#' + r.toString(16) + g.toString(16) + b.toString(16);
+    event.target.style.borderBottomColor = color;
+}
+
+changeColor = (event) =>
+{
+    r = Math.floor(Math.random() * (256)),
+    g = Math.floor(Math.random() * (256)),
+    b = Math.floor(Math.random() * (256)),
+    color = '#' + r.toString(16) + g.toString(16) + b.toString(16);
+    event.target.style.background = color;
+}
+
+dissapearFigure = (event) =>
+{
+    event.target.style.display = 'none';
+}
